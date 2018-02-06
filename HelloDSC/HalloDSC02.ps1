e8e5d3cc2879f768c50c3264c1c24c40e64f6c47
Peter Monadjemi <pm@activetraining.de>
2018-02-06 21:14:03 +0100

    changed name of general directory

---
+++ HelloDSC/HalloDSC02.ps1
@@ -0,0 +1,36 @@
+﻿<#
+ .Synopsis
+ A simple DSC example for creating a directory and a file on drive C: on the local computer
+ #>
+
+configuration HalloDSC
+{
+    Import-DscResource –ModuleName PSDesiredStateConfiguration
+
+    node localhost
+    {
+        # Create a directory
+        File TestDir
+        {
+            Ensure = "Present"
+            DestinationPath = "C:\PoshScripts"
+            Type = "Directory"
+        }
+
+        # Create a file
+        File TestFile
+        {
+            Ensure = "Present"
+            DestinationPath = "C:\PoshScripts\Test.ps1"
+            Type = "File"
+            Contents = "`"The current time is: $(Get-Date -Format t)`""
+        }
+    }
+
+ }
+
+# Generate a mof file localhost.mof
+# HalloDSC
+
+# Apply the configuration to the local computer
+# Start-DscConfiguration -Path .\HalloDSC -Verbose -Wait
---
+++ HelloDSC/PoshConfigBeispiel.ps1
@@ -0,0 +1,59 @@
+﻿<#
+ .Synopsis
+ Creating a user profile file on a remote computer with DSC
+#>
+
+configuration PoshConfig
+{
+    param([String[]]$Computername, 
+          [Parameter(Mandatory=$true)][String]$Username)
+
+    $Ps1Content = "# A sample for a powershell profile script`n"
+    $Ps1Content += "`$PsHost.PrivateData.ErrorBackgroundColor = 'White'`n"
+    $Ps1Content += "cd `$env:userprofile\documents\windowspowershell`n"
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    node $Computername
+    {
+        # Create a directory for command line tools
+        File ToolsDir
+        {
+            Ensure = "Present"
+            DestinationPath = "$env:userprofile\documents\tools"
+            Type = "Directory"
+        }
+
+        # Create the Windows PowerShell directory
+        File Ps1Dir
+        {
+            Ensure = "Present"
+            DestinationPath = "C:\Users\$Username\Documents\WindowsPowerShell"
+            Type = "Directory"
+        }
+
+        # Create the profile file
+        File Ps1File
+        {
+            Ensure = "Present"
+            DependsOn = "[File]Ps1Dir"
+            DestinationPath = "$env:userprofile\documents\WindowsPowerShell\Profile.ps1"
+            Contents = $Ps1Content
+            Type = "File"
+        }
+
+        # Append the tools directory to the path environment variable
+        Environment Tools
+        {
+            Ensure = "Present"
+            Name = "Path"
+            Path = $true
+            Value = "$env:userprofile\documents\tools"
+        }
+    }
+
+}
+
+# PoshConfig -Computername Localhost -Username PsHub-Admin
+
+# Start-DscConfiguration -Path .\PoshConfig -Wait -Verbose -Force
